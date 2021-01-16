2ab2e2e7fc206d2063be486eaf9b9e714e71acaa
Uzair Ashraf <uzinatorcl@live.com>
2021-01-16 15:02:38 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Gundam Store
+
+A web application to replicate a small version of the usa gundam store
---
+++ data.js
@@ -0,0 +1,26 @@
+// Do not write any code here
+
+var data = {
+  cart: [],
+  favorites: [],
+  products: [
+    {
+      id: 1
+      name: 'Wing Gundam Zero',
+      price: 55.99,
+      imageUrl: './images/wing-gundam-zero.png'
+    },
+    {
+      id: 2
+      name: 'Neo Zeon Sazabi',
+      price: 92.99,
+      imageUrl: './images/neo-zeon-sazabi.jpg'
+    },
+    {
+      id: 3
+      name: 'Gundam Sandrock',
+      price: 41.99,
+      imageUrl: './images/gundam-sandrock.jpg'
+    }
+  ]
+}
